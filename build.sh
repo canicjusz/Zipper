0d68dc55864cf0d5c3eee921cf3117ddf7014906
canicjusz <janek.bananek371@gmail.com>
2020-10-31 13:23:12 +0100

    added license and changed file name

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ cp zipper_64.png zipper_128.png manifest.webapp dist
 cd dist
 zip -q ../application.zip  css/* index.html js/* zipper_64.png zipper_128.png manifest.webapp
 cd ..
-zip -q zipper.zip application.zip metadata.json update.webapp
+zip -q Zipper.zip application.zip metadata.json update.webapp
