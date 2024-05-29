54e1d66a0354801659c80a3fe237f7e4d42eb7e5
canicjusz <janek.bananek371@gmail.com>
2024-05-29 13:23:50 +0200

    clean up the root directory

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 rm -rf application.zip
-rm -rf zipper.zip
+rm -rf Zipper.zip
 npm run build
-cp zipper_64.png zipper_128.png manifest.webapp dist
+cp assets/zipper_64.png assets/zipper_128.png assets/manifest.webapp dist
 cd dist
 zip -q ../application.zip  css/* index.html js/* zipper_64.png zipper_128.png manifest.webapp
 cd ..
-zip -q Zipper.zip application.zip metadata.json update.webapp
+zip -qj Zipper.zip application.zip assets/metadata.json assets/update.webapp
 rm -rf application.zip
